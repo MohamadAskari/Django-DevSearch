16e42abed1bead2582e41ba3aa3d23fa03155059
MohamadAskari <mohamad_ask@aut.ac.ir>
2021-09-27 00:46:53 +0330

    update

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -145,9 +145,9 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 
 
